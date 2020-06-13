62d57630e6cdb112b5f5913db1474f74f7a7925d
azabraao <az.abraao@gmail.com>
2020-06-13 13:06:45 -0300

    Lida com validação de formulário

--- src/app/auth/auth-login/auth-login.component.ts
+++ src/app/auth/auth-login/auth-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-auth-login",
@@ -6,7 +7,60 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./auth-login.component.scss"],
 })
 export class AuthLoginComponent implements OnInit {
-  constructor() {}
+  public loginForm: FormGroup;
+  public data: any = {};
+  public formFeedback: string = "";
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
 
   ngOnInit() {}
+
+  async sendLogin() {
+    let formValue = this.loginForm.value;
+
+    console.log(formValue);
+
+    if (!this.loginForm.valid) {
+      alert("Preencha os campos corretamente.");
+
+      return;
+    } else {
+      alert("sem erros");
+    }
+
+    console.log(this.loginForm.value);
+  }
+
+  getFieldError(formControlName) {
+    let control = this.loginForm.get(formControlName);
+
+    if (!control) return false;
+
+    if (control.untouched) return false;
+
+    if (control.valid) return false;
+
+    if (control.hasError("required")) {
+      return "Este campo é obrigatório.";
+    }
+
+    if (control.hasError("minlength")) {
+      let minlength = control.getError("minlength");
+      return (
+        "O mínimo de caracteres para este campo é " +
+        minlength.requiredLength +
+        "."
+      );
+    }
+
+    return "Campo inválido.";
+  }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AuthLoginComponent } from "./auth-login/auth-login.component";
-import { MatInputModule } from "@angular/material/input";
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/button";
-import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AuthLoginComponent],
-  imports: [
-    CommonModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatFormFieldModule,
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class AuthModule {}
