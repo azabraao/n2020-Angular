fef959fefc9fc28718acab7900bf4bdfd77ee864
azabraao <az.abraao@gmail.com>
2020-06-16 02:08:43 -0300

    Adiciona estilo na página novo-video

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from "@angular/common";
 import { InsideLayoutComponent } from "./inside-layout/inside-layout.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { RouterModule } from "@angular/router";
-import { HomeComponent } from './home/home.component';
-import { NovoVideoComponent } from './novo-video/novo-video.component';
+import { HomeComponent } from "./home/home.component";
+import { NovoVideoComponent } from "./novo-video/novo-video.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [InsideLayoutComponent, NavbarComponent, HomeComponent, NovoVideoComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    InsideLayoutComponent,
+    NavbarComponent,
+    HomeComponent,
+    NovoVideoComponent,
+  ],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
 })
 export class LayoutModule {}
--- src/app/layout/novo-video/novo-video.component.ts
+++ src/app/layout/novo-video/novo-video.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-novo-video',
-  templateUrl: './novo-video.component.html',
-  styleUrls: ['./novo-video.component.scss']
+  selector: "app-novo-video",
+  templateUrl: "./novo-video.component.html",
+  styleUrls: ["./novo-video.component.scss"],
 })
 export class NovoVideoComponent implements OnInit {
+  public novoVideoForm: FormGroup;
+  public data: any = {};
+  public formFeedback: string = "";
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private authService: AuthService, private router: Router) {
+    this.novoVideoForm = new FormGroup({
+      link: new FormControl("", [Validators.required]),
+      titulo: new FormControl("", [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
 
+  ngOnInit() {}
+
+  async sendVideo() {
+    let formValue = this.novoVideoForm.value;
+
+    console.log(formValue);
+
+    if (!this.novoVideoForm.valid) {
+      console.log("form invalid");
+      return;
+    } else {
+      console.log("form invalido");
+    }
+
+    console.log(this.novoVideoForm.value);
+  }
+
+  resetFeedback() {
+    this.formFeedback = "";
+  }
+
+  getFieldError(formControlName) {
+    let control = this.novoVideoForm.get(formControlName);
+
+    if (!control) return false;
+
+    if (control.untouched) return false;
+
+    if (control.valid) return false;
+
+    if (control.hasError("required")) {
+      return "Este campo é obrigatório.";
+    }
+
+    if (control.hasError("minlength")) {
+      let minlength = control.getError("minlength");
+      return (
+        "O mínimo de caracteres para este campo é " +
+        minlength.requiredLength +
+        "."
+      );
+    }
+
+    return "Campo inválido.";
+  }
 }
