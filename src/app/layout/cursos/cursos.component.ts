6950d5edb9a0b8db44f56ba7f75ecfa98a29a5e3
azabraao <az.abraao@gmail.com>
2020-06-17 18:13:33 -0300

    Adiciona integração na listagem de cursos

--- src/app/layout/cursos/cursos.component.ts
+++ src/app/layout/cursos/cursos.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CursoService } from "src/app/services/curso.service";
 
 @Component({
-  selector: 'app-cursos',
-  templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.scss']
+  selector: "app-cursos",
+  templateUrl: "./cursos.component.html",
+  styleUrls: ["./cursos.component.scss"],
 })
 export class CursosComponent implements OnInit {
+  public cursos: any = [];
 
-  constructor() { }
+  constructor(public cursoService: CursoService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    try {
+      this.cursos = await this.cursoService.getCursos();
+    } catch (error) {
+      console.error(error);
+    }
   }
-
 }
